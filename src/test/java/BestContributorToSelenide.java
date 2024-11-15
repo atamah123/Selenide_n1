0b66e0fdc0f40be5b425299ddff424c3446f2ef1
Sergey Atamanchuk <atamah123@gmail.com>
2024-11-15 10:18:09 +0300

    Второй класс добавлен

---
+++ src/test/java/BestContributorToSelenide.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributorToSelenide {
+
+    @Test
+    void andreiSolntsevShouldBeTheFirstContributor () {
+        // открыть страницу репозитория селенида
+        open("https://github.com/selenide/selenide/");
+        // подвести мышку к первому аватару из блока contributors
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                .closest(".BorderGrid-cell").$$("ul li").first().hover();
+        // проверка: во всплывающем окне есть текст Andrei Solntsev
+        $$(".Popover").findBy(visible).shouldHave(text("zalupa"));
+        sleep(5000);
+
+    }
+
+}
--- src/test/java/SelenideRepositorySearch.java
+++ src/test/java/SelenideRepositorySearch.java
@@ -1,10 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepositorySearch {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+        Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
-        
+        // открыть главную страницу
+        open("https://www.google.com/");
+        // ввести в поле поиска selenide и нажать enter
+       $("[name=q]").setValue("selenide").pressEnter();
+       // кликнуть на первый репозиторий из списка найденых
+//        $$
+//       sleep(5000); 40:30
     }
 
 }
